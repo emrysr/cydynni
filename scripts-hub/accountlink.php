cd96e778860d0c79ee08174f67d10bbf7bed221b
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-03-28 16:03:20 +0100

    full account creation

--- scripts-hub/accountlink.php
+++ scripts-hub/accountlink.php
@@ -1,12 +1,13 @@
 <?php
 
 $emoncms_userid = 1;
-$emoncms_username = "";
-$emoncms_password = "";
+$emoncms_username = readline("emoncms_username: ");
+$emoncms_password = readline("emoncms_password: ");
 
 define('EMONCMS_EXEC', 1);
 chdir("/var/www/emoncms");
 require "process_settings.php";
+require "Modules/user/user_model.php";
 
 // Connect to MYSQL
 $mysqli = @new mysqli($server,$username,$password,$database,$port);
@@ -18,9 +19,11 @@ if ( $mysqli->connect_error ) {
     die();
 }
 
+$user = new User($mysqli,false);
+
 // Send request
 $ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,"https://emoncms.cydynni.org.uk/user/auth.json");
+curl_setopt($ch,CURLOPT_URL,"https://emoncms.org/user/auth.json");
 curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_POSTFIELDS,"username=$emoncms_username&password=$emoncms_password");
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
@@ -28,16 +31,24 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 $result = json_decode($result);
-
 if ($result!=null && isset($result->success) && $result->success) {
 
     // Fetch full account details from remote emoncms
-    $u = json_decode(file_get_contents("https://emoncms.cydynni.org.uk/user/get.json?apikey=".$result->apikey_write));
+    $u = json_decode(file_get_contents("https://emoncms.org/user/get.json?apikey=".$result->apikey_write));
+    
+    // Register account locally
+    $result = $user->register($username, $password, $u->email);
     
     // Save remote account apikey to local hub
-    if ($result->success==true) {
-        $mysqli->query("UPDATE users SET apikey_write = '".$u->apikey_write."' WHERE id='$emoncms_userid'");
-        $mysqli->query("UPDATE users SET apikey_read = '".$u->apikey_read."' WHERE id='$emoncms_userid'");
+    if ($result['success']==true) {
+        $userid = $result['userid'];
+        $mysqli->query("UPDATE users SET apikey_write = '".$u->apikey_write."' WHERE id='$userid'");
+        $mysqli->query("UPDATE users SET apikey_read = '".$u->apikey_read."' WHERE id='$userid'");
+        
+        $emonhubconf = file_get_contents("/home/pi/data/emonhub.conf");
+        $emonhubconf = str_replace($emonhubconf,"apikey = xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx","apikey = ".$u->apikey_write);
+        $fh = fopen("/home/pi/data/emonhub.conf","w");        
+        fwrite($fh,$emonhubconf);                
+        fclose($fh);
     }
 }
-                   
